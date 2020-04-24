2649378618ebae6a7941e7b4959824fe38862570
cortexA233 <39519037+cortexA233@users.noreply.github.com>
2020-04-24 16:03:41 +0800

    encode_zipped updated

--- encode_zipped.lua
+++ encode_zipped.lua
@@ -1,56 +1,57 @@
 ------------------此文件用给定table构造CyBuf格式数据----------------
 require("test")
 
----------------encode主函数---------------------
-function encode(map,tab_count)
+
+
+---------------encode_zipped主函数---------------------
+function encode_zipped(map,tab_count,optional_divider)
   
   if(tab_count==nil) then
     tab_count=0
   end
-  
+  if(optional_divider==nil) then
+    optional_divider=''
+  end
   local cybuf_str=''
   ---------------默认最外层不带大括号-----------
   for i=1,tab_count-1 do
-    cybuf_str=cybuf_str..'\t'
+  --  cybuf_str=cybuf_str..'\t'
   end
   tab_count=tab_count+1
   
   if(tab_count>1) then
-    cybuf_str=cybuf_str.."{\n"
+    cybuf_str=cybuf_str.."{"
   end
   
   for i,v in pairs(map) do
     
     for i=2,tab_count do
-      cybuf_str=cybuf_str..'\t'
+    --  cybuf_str=cybuf_str..'\t'
     end
     
     if(type(v)~="table") then
-      cybuf_str=cybuf_str..tostring(i)..': '
+      cybuf_str=cybuf_str..tostring(i)..':'
       if(type(v)=="string") then
         cybuf_str=cybuf_str..'"'..tostring(v)..'"'
       else
-        cybuf_str=cybuf_str..tostring(v)
+        cybuf_str=cybuf_str..tostring(v)..' '
       end
-      cybuf_str=cybuf_str..'\n'
+    --  cybuf_str=cybuf_str..'\n'
       
     else
-      cybuf_str=cybuf_str..tostring(i)..': \n'
-      cybuf_str=cybuf_str..encode(v,tab_count)
+      cybuf_str=cybuf_str..tostring(i)..':'
+      cybuf_str=cybuf_str..encode_zipped(v,tab_count)
     end
   end
   
   for i=1,tab_count-2 do
-    cybuf_str=cybuf_str..'\t'
+  --  cybuf_str=cybuf_str..'\t'
   end
   ---------------默认最外层不带大括号-----------
   if(tab_count>1) then
-    cybuf_str=cybuf_str..'}\n'
+    cybuf_str=cybuf_str..'}'
   end
-  --------------文件末尾无多余空行------------
-  if(tab_count<=1) then
-    return cybuf_str.sub(cybuf_str,1,#cybuf_str-1)
-  end
+  
   return cybuf_str
   
 end
@@ -71,6 +72,6 @@ a["school"]["major"]["class"]="engineering"
 
 print("------------------↓↓↓  分割线：encode文件自带测试内容  ↓↓↓-------------------")
 
-print(encode_test(a))
+print(encode_zipped(a))
 
 print("------------------↑↑↑  分割线：encode文件自带测试内容  ↑↑↑-------------------\n")
